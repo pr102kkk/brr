809d22ec700d1519981a6de0d3b4fc2a9cc483a4
Ozike <>
2022-03-19 13:24:42 +0700

    koment3

--- app/src/main/java/com/example/pojecgitbezborodovabauyrova/MainActivity.java
+++ app/src/main/java/com/example/pojecgitbezborodovabauyrova/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+//tttt
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
